12089f6ce76e77849ca756f2ea35ff12553dd57f
44lr <73897958+44lr@users.noreply.github.com>
2021-11-28 03:48:40 -0300

    init

---
+++ index.js
@@ -0,0 +1,173 @@
+const Discord = require("discord.js");
+const { Resolver } = require("dns");
+const Express = require("express");
+const REST = Express();
+const http = require('http');
+const NodeCache = require("node-cache")
+
+const Cache = new NodeCache();
+const Config = require("./config.json");
+const Package = require("./package.json");
+const Client = new Discord.Client( { intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_WEBHOOKS"] });
+const DateObj = new Date();
+
+var aMsgCache = [];
+
+/*
+    Logger
+*/
+const LogType = {
+	Discord: "\x1b[31m [Discord] \x1b[39m",
+	Chat: "\x1b[36m [Chat] \x1b[39m",
+    Rest: "\x1b[95m [REST] \x1b[39m",
+    Error: "\x1b[31m [Error] \x1b[39m"
+};
+
+function WriteLog( eLogType, sLogMessage ) {
+	if (!eLogType in LogType)
+		throw new error("invalid log type");
+
+	var sHours = DateObj.getHours();
+	var sMinutes = DateObj.getMinutes();
+	var sSeconds = DateObj.getSeconds();
+	console.log(sHours + ":" + sMinutes + ":" + sSeconds + " -" + eLogType + sLogMessage);
+};
+
+/*
+    Webhook
+*/
+var Webhook = null;
+
+async function SendMessage(sAvatarUrl, sName, sContent) {
+    Webhook.send({
+        username: sName,
+        content: sContent,
+        avatarURL: sAvatarUrl
+    });
+}
+
+/*
+    Discord bot
+*/
+
+Client.on("ready", async () => {
+    WriteLog(LogType.Discord, "client ready, initializing bot");
+
+    var MsgChannel = Client.channels.cache.get(Config.ChannelId);
+    
+    if (!MsgChannel)
+        throw new Error("invalid channel id");
+
+    WriteLog(LogType.Discord, "reading and sending messages to channel: " + MsgChannel.name)
+
+    var ChannelPerms = MsgChannel.guild.me.permissionsIn(MsgChannel);
+
+    if (!ChannelPerms.has("VIEW_CHANNEL"))
+        throw new error("not allowed to view channel");
+
+    if (!ChannelPerms.has("MANAGE_WEBHOOKS"))
+        throw new Error("not allowed to manage webhooks");
+
+    if (!ChannelPerms.has("SEND_MESSAGES"))
+        throw new Error("not allowed to send messages");
+
+    var Webhooks = await MsgChannel.fetchWebhooks();
+
+    WriteLog(LogType.Discord, "getting webhook");
+
+    Webhook = Webhooks.find(TmpWebhook => TmpWebhook.name == "GDR");
+    if (!Webhook) {
+        WriteLog(LogType.Discord, "webhook does not exist, creating a new one");
+        MsgChannel.createWebhook("GDR", {
+            avatar: "",
+            reason: "GM Discord Relay webhook"
+        });
+    };
+
+    WriteLog(LogType.Discord, "bot ready");
+});
+
+Client.on("messageCreate", Message => {
+    if (Message.author.bot) return;
+    if (Message.channelId != Config.ChannelId) return;
+
+    WriteLog(LogType.Chat, Message.author.tag + ": " + Message.content);
+    aMsgCache.push([
+        Message.author.tag,
+        Message.content
+    ]);
+});
+
+Client.on("error", (sError) => {
+    WriteLog(LogType.Error, "client error:" + sError);
+});
+
+Client.login(Config.Token);
+
+/*
+    Steam avatar stuff
+*/
+async function GetAvatar(sSID64) {
+    var sAvatarURL = Cache.get(sSID64);
+    if (sAvatarURL)
+        return sAvatarURL;
+
+    var ReqOptions = {
+        hostname: "api.steampowered.com",
+        path: "/ISteamUser/GetPlayerSummaries/v0002/?key=" + Config.SteamKey + "&steamids=" + sSID64,
+        method: "GET"
+    };
+
+    var ReqPromise = new Promise( (Resolve, Reject) => {
+        var sResponse = "";
+        http.request(ReqOptions, Response => {
+            Response.on("data", Data => {
+                sResponse += Data;
+            });
+
+            Response.on("end", () => {
+                Resolve(JSON.parse(sResponse));
+            });
+
+            Response.on("error", sError => {
+                WriteLog(LogType.Error, "steam api request failed:" + sError);
+                Reject(sError);
+            })
+        }).end();
+    });
+
+    var Response = await ReqPromise;
+    Cache.set(sSID64, Response.response.players[0].avatarfull);
+    return Response.response.players[0].avatarfull;
+};
+
+/*
+    Rest server
+*/
+REST.get("/getmessages", (Request, Response) => {
+    if (Request.hostname != Config.SrcdsIp) {
+        Response.end();
+        return;
+    };
+    
+    Response.send(JSON.stringify(aMsgCache));
+    aMsgCache = [];
+});
+
+REST.use(Express.json())
+REST.post("/sendmessage", async (Request, Response) => {
+    if (Request.hostname != Config.SrcdsIp) {
+        Response.end();
+        return;
+    };
+
+    var MsgInfo = Request.body;
+    Response.end();
+
+    var sAvatar = await GetAvatar(MsgInfo[0]);
+    SendMessage(sAvatar, MsgInfo[1], MsgInfo[2]);
+});
+
+var Server = REST.listen(Config.Port, () => {
+    WriteLog(LogType.Rest, "server ready, listening on port " + Server.address().port);
+});
---
+++ readme.md
@@ -0,0 +1,20 @@
+# GMod / Discord relay
+Allow Discord users to talk with in-game users on Garry's mod.
+
+## Requirements
+- [NodeJS](https://nodejs.org/en/download/).
+- A Discord server and a Garry's Mod server.
+- A [Steam API key](https://steamcommunity.com/dev/apikey).
+- A Discord channel's ID. (Which you can get following [this guide]((https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-))
+- A Discord bot, which has been invited to your server and has the following permissions:  Manage Webhooks, Send Messages and View Channel.
+
+## How to install
+1 - Clone both the [addon](https://github.com/44lr/gdr_addon) and the [node server](https://github.com/44lr/gdr_nodesv).\
+2 - Copy the addon into your server's addons folder.\
+3 - on gdr_addon/lua/gdr/sv_config.lua, set your settings accordingly\
+4 - on gdr_nodesv/config.json, also set your settings accordingly.\
+5 - On a terminal / cmd instance, cd into the folder where you downloaded gdr_nodesv and run **npm install**\
+
+## How to run
+1 - On a terminal / cmd instance, cd into where you downloaded gdr_nodesv, and run **node index.js**\
+2 - Run your Garry's Mod server.
